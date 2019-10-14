81895397cc616efb27e36663c6164b2ed608109c
hawk <andreas.stroot@gmx.de>
2019-10-14 18:05:08 +0200

    codereview playground square

--- student/JAVA_LABS/src/main/java/de/gedoplan/seminar/java/exercise/basic/Playground.java
+++ student/JAVA_LABS/src/main/java/de/gedoplan/seminar/java/exercise/basic/Playground.java
@@ -2,14 +2,12 @@ package de.gedoplan.seminar.java.exercise.basic;
 
 public class Playground {
 
-	public static void main(String[] args) {
-		int i = 1;
-		while (i <= 10) {
-			System.out.print("Die Quadratzahl von " + i + " ist: ");
-			System.out.println(i * i);
-			i++;
-		}
-
-	}
-
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
+            int quadrat = i * i;
+            System.out.println("Die Quadratzahl von " + i + " ist: " + quadrat);
+            i++;
+        }
+    }
 }
