28141a4821f3460b90550c5a480eb8e4e395fd19
matthias <m@m.de>
2019-10-15 10:03:39 +0200

    iteration over main args

--- student/JAVA_LABS/src/main/java/de/gedoplan/seminar/java/exercise/basic/Playground.java
+++ student/JAVA_LABS/src/main/java/de/gedoplan/seminar/java/exercise/basic/Playground.java
@@ -3,11 +3,8 @@ package de.gedoplan.seminar.java.exercise.basic;
 public class Playground {
 
     public static void main(String[] args) {
-        int i = 1;
-        while (i <= 10) {
-            int quadrat = i * i;
-            System.out.println("Die Quadratzahl von " + i + " ist: " + quadrat);
-            i++;
+        for (String element : args) {
+        	System.out.println(element);
         }
     }
 }
