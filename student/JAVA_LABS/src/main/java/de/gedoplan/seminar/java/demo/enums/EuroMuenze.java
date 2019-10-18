54c3d23eb8e76c034c32b2860e4c84052c629834
matthias <m@m.de>
2019-10-18 10:01:27 +0200

    add enum euromuenzen

---
+++ student/JAVA_LABS/src/main/java/de/gedoplan/seminar/java/demo/enums/EuroMuenze.java
@@ -0,0 +1,5 @@
+package de.gedoplan.seminar.java.demo.enums;
+
+public enum EuroMuenze {
+	cent1, cent2, cent5, cent10, cent20, cent50, euro1, euro2
+}
---
+++ student/JAVA_LABS/src/main/java/de/gedoplan/seminar/java/demo/enums/ListEuroMuenze.java
@@ -0,0 +1,12 @@
+package de.gedoplan.seminar.java.demo.enums;
+
+public class ListEuroMuenze {
+
+	public static void main(String[] args) {
+		for (EuroMuenze muenze: EuroMuenze.values()) {
+			System.out.println(muenze);
+		}
+
+	}
+
+}
