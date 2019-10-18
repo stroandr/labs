883de30429c6b6c13e29800ca2591e7774793b5b
matthias <m@m.de>
2019-10-18 10:05:03 +0200

    pretty print

--- student/JAVA_LABS/src/main/java/de/gedoplan/seminar/java/demo/enums/ListEuroMuenze.java
+++ student/JAVA_LABS/src/main/java/de/gedoplan/seminar/java/demo/enums/ListEuroMuenze.java
@@ -3,7 +3,7 @@ package de.gedoplan.seminar.java.demo.enums;
 public class ListEuroMuenze {
 
 	public static void main(String[] args) {
-		for (EuroMuenze muenze: EuroMuenze.values()) {
+		for (EuroMuenze muenze : EuroMuenze.values()) {
 			System.out.println(muenze);
 		}
 
