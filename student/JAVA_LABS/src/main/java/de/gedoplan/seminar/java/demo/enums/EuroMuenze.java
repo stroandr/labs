080c58d97b2ae11eedf72acc5076ab554fb08fec
matthias <m@m.de>
2019-10-18 15:44:21 +0200

    end of day five

--- student/JAVA_LABS/src/main/java/de/gedoplan/seminar/java/demo/enums/EuroMuenze.java
+++ student/JAVA_LABS/src/main/java/de/gedoplan/seminar/java/demo/enums/EuroMuenze.java
@@ -1,5 +1,5 @@
 package de.gedoplan.seminar.java.demo.enums;
 
 public enum EuroMuenze {
-	cent1, cent2, cent5, cent10, cent20, cent50, euro1, euro2
+	CENT1, CENT2, CENT5, CENT10, CENT20, CENT50, EURO1, EURO2
 }
