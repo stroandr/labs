b3ed6b3394b3db77ea7872b08b1e140d3a1dc8eb
hawk <andreas.stroot@gmx.de>
2019-11-03 18:32:47 +0100

    add MainDemo.java with simple for loop

--- student/JAVA_LABS/src/main/java/de/gedoplan/seminar/java/demo/basic/MainDemo.java
+++ student/JAVA_LABS/src/main/java/de/gedoplan/seminar/java/demo/basic/MainDemo.java
@@ -5,12 +5,12 @@ package de.gedoplan.seminar.java.demo.basic;
  */
 public class MainDemo {
 
-  public static void main(String[] args) {
-    System.out.println("Anzahl Argumente: " + args.length);
-    System.out.println("Erstes Element: " + args[0]);
-    System.out.println("Zweites Element: " + args[1]);
-//    for (int x=0, args.length, x++) {
-//    	System.out.println("Element " + x + ": " + args[x]);
-//    }
-  }
+    public static void main(String[] args) {
+        System.out.println("Anzahl Argumente: " + args.length);
+        System.out.println("Erstes Element: " + args[0]);
+        System.out.println("Zweites Element: " + args[1]);
+    for (int x=0; x < args.length; x++) {
+    	System.out.println("Element " + x + ": " + args[x]);
+    }
+    }
 }
